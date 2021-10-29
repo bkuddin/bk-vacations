c601a25826b1a9f9510f82d462f5d298ccdff495
BK Uddin <1bkhossain@gmail.com>
2021-10-29 19:48:14 +0600

    Defiend env local

--- src/components/Firebase/firebase.config.js
+++ src/components/Firebase/firebase.config.js
@@ -1,10 +1,10 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyA0Ryhyorkt8-TzAcl5UX4PBGTne_mcV9Q",
-  authDomain: "bk-vacations.firebaseapp.com",
-  projectId: "bk-vacations",
-  storageBucket: "bk-vacations.appspot.com",
-  messagingSenderId: "458063536938",
-  appId: "1:458063536938:web:fe5b363f79d30698b5b453",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 export default firebaseConfig;
