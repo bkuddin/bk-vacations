eb6b9e758bd663e80e7f29c70f3632a1c07841a0
BK Uddin <1bkhossain@gmail.com>
2021-10-29 19:37:11 +0600

    Installed Firebase and Configured

---
+++ src/components/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA0Ryhyorkt8-TzAcl5UX4PBGTne_mcV9Q",
+  authDomain: "bk-vacations.firebaseapp.com",
+  projectId: "bk-vacations",
+  storageBucket: "bk-vacations.appspot.com",
+  messagingSenderId: "458063536938",
+  appId: "1:458063536938:web:fe5b363f79d30698b5b453",
+};
+
+export default firebaseConfig;
